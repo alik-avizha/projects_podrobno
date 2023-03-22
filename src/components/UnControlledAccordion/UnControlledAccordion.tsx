baaefb503447df91e69e9c2284c0be2752ee59b8
Alik_Avizha <aleksandravizha@icloud.com>
2023-03-22 12:06:47 +0300

    first commit

---
+++ src/components/UnControlledAccordion/UnControlledAccordion.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean;
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log('Accordion rendering')
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            {!props.collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionTitlePropsTitle = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsTitle) {
+    console.log('AccordionTitle rendering')
+    return (
+        <>
+            <h3>--{props.title}--</h3>
+        </>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Accordion;
