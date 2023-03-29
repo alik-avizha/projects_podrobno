7752a41506ba91eba186f60095e6af9f70b48196
Alik_Avizha <aleksandravizha@icloud.com>
2023-03-29 19:46:47 +0300

    add Storybook

---
+++ src/components/UnControlledAccordion/UnControlledAccordion.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import UnControlledAccordion from './UnControlledAccordion';
+
+export default {
+    title: 'UnControlledAccordion',
+    component: UnControlledAccordion
+}
+
+
+export const UnControlledMenuCollapsedMode = () => <UnControlledAccordion  titleValue={'UnControlled Menu'}/>;
+
+
+
---
+++ src/components/UnControlledReating/UnControlledRating.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {UnControlledRating} from './UnControlledReating';
+
+export default {
+    title: 'UnControlledRating',
+    component: UnControlledRating
+}
+
+
+export const ChangeRating = () => <UnControlledRating  />;
+
+
+
