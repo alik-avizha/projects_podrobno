52510977c7647487f5ca00604f43eb404615688a
Alik_Avizha <aleksandravizha@icloud.com>
2023-04-05 12:55:02 +0300

    work with controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -35,3 +35,40 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
             - action value: {value}
         </>)
 }
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>)=> {setParentValue(e.currentTarget.value)}
+
+    return (
+        <input value={parentValue} onChange={onChangeHandler}/>
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>)=> {setParentValue(e.currentTarget.checked)}
+
+    return (
+        <input type='checkbox' checked={parentValue}  onChange={onChangeHandler}/>
+    )
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>)=> {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return (
+        <select value={parentValue} onChange={onChangeHandler}>
+            <option>none</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Moscow</option>
+            <option value={'3'}>Kiev</option>
+        </select>
+    )
+}
