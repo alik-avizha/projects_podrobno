0c84531859bb859a0ad8340e2d93394f45253b85
Alik_Avizha <aleksandravizha@icloud.com>
2023-04-18 22:42:32 +0300

    work with useMemo

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,76 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: 'useMemo'
+};
+
+export const DifficultCountingExample = () => {
+
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(()=>{
+        let tempResultA = 1;
+        for (let i = 1; i <= a ; i++) {
+            let fake = 0
+            while (fake < 1000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA
+    },[a])
+
+    for (let i = 1; i <= b ; i++) {
+        resultB = resultB * i
+    }
+
+    return (
+        <>
+            <input value={a} onChange={(e)=>setA(+e.currentTarget.value)}/>
+            <input value={b} onChange={(e)=>setB(+e.currentTarget.value)}/>
+            <hr/>
+            <div>
+                Result for a: {resultA}
+            </div>
+            <div>
+                Result for b: {resultB}
+            </div>
+        </>
+        )
+
+}
+
+const UsersSecret = (props: {users: Array<string>}) => {
+    console.log('UsersSecret')
+    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log('HelpsToReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Alik', 'Igor', 'Artem'])
+
+    const newArr = useMemo(()=>{
+        return users.filter(u => u.toLowerCase().indexOf("a") > -1)
+    },[users])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add User</button>
+        {counter}
+        <Users users={newArr}/>
+    </>
+}
+
