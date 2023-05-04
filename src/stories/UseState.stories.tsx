b5a8b0488ab1100b88b8eba537c6eb368c88bd4e
Alik_Avizha <aleksandravizha@icloud.com>
2023-05-04 13:49:36 +0300

    work with useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'useState demo'
+}
+
+function generateData() {
+    //difficult counting
+    console.log('generateData')
+    return 1
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+
+    //const initValue = useMemo(generateData, [])
+
+    const [counter, setCounter] = useState(generateData)
+
+    //const changer = (state: number) => state + 1
+
+    return <>
+        <button onClick={() => setCounter(state => state + 1)}>+</button>
+        {counter}
+    </>
+}
